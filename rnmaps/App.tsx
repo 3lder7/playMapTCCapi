b010108b744f68b582ef52e4c169bd90d8be669a
Elder Galvão <114260709+3lder7@users.noreply.github.com>
2024-10-09 19:05:04 +0000

    APP FUNCIONANDO NO RNMAPS

--- rnmaps/App.tsx
+++ rnmaps/App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './src/LoginScreen';
+import CadastroScreen from './src/CadastroScreen';
+import MapaScreen from './src/MapaScreen';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+function App(): React.JSX.Element {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Cadastro" component={CadastroScreen} />
+        <Stack.Screen name="Mapa" component={MapaScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ rnmaps/src/MapaScreen.tsx
@@ -0,0 +1,36 @@
+import { View } from "react-native";
+import { styles } from './stylesMap/styles';//estilos da tela
+import { requestForegroundPermissionsAsync} from "expo-location";//pedir permissão ao usuario para usar localização // assincrono
+import { getCurrentPositionAsync} from "expo-location";//obter a localização do usuario
+import { LocationObject } from "expo-location";//latitude e longitude
+import { useEffect, useState } from "react";
+//import MapView  from "react-native-maps";//importa para a visualização do mapa
+
+export default function MapScreen(){
+
+    const [location, setLocation] = useState <LocationObject | null>(null);
+
+    //PEDINDO LOCALIZAÇÃO-------------------------
+    async function requestLocationPermission(){
+       const { granted } = await requestForegroundPermissionsAsync();//verifca a permissão e aguarda a resposta
+
+       if(granted){
+            const posicaoAtual = await getCurrentPositionAsync();//assincrono
+            setLocation(posicaoAtual);
+
+            console.log("LOCALIZAÇÃO ATUAL =>", posicaoAtual);//exibe no terminal a loc
+       }
+    }
+
+    useEffect(() =>{//usando a função//no momento que a intrface é renderizada, solicita o método
+        requestLocationPermission();
+    }, []);
+
+    return (
+        <View style ={styles.container}>
+            
+        
+
+        </View>
+    );
+}
