fbb78e4a9e5abec5221f560b7609ab9c5d99bae2
CODE-V2005 <141840061+CODE-V2005@users.noreply.github.com>
2024-10-28 16:07:41 -0300

    Atualização no App.tsx
    Criando um menu para as novas telas de Grupos, Eventos e Perfil com a dependência Bottomtabnavigator do proprio react native

--- App.tsx
+++ App.tsx
@@ -1,26 +1,54 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack'; // Use isso
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 import LoginScreen from './src/LoginScreen';
 import CadastroScreen from './src/CadastroScreen';
 import MapScreen from './src/MapScreen';
+import GroupsScreen from './src/GroupsScreen';
+import EventsScreen from './src/EventsScreen';
+import ProfileScreen from './src/ProfileScreen';
 
-const Stack = createNativeStackNavigator(); // Atualize aqui
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
-function App(): React.JSX.Element {
+// Menu para as telas principais criado com a dependência BottomTabNavigator
+function MainTabNavigator() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          position: 'absolute',
+          bottom: 0,
+          height: 60,
+          borderTopWidth: 0,
+          elevation: 0,
+          backgroundColor: '#fff',
+        },
+        headerShown: true,
+      }}
+    >
+      <Tab.Screen name="Home" component={MapScreen} />
+      <Tab.Screen name="Grupos" component={GroupsScreen} />
+      <Tab.Screen name="Eventos" component={EventsScreen} />
+      <Tab.Screen name="Perfil" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+}
+
+export default function App(): React.JSX.Element {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Cadastro">
+        <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Cadastro" component={CadastroScreen} />
-          <Stack.Screen name="Mapa" component={MapScreen} />
+          <Stack.Screen name="Main" component={MainTabNavigator} />
         </Stack.Navigator>
       </NavigationContainer>
     </GestureHandlerRootView>
   );
 }
-
-export default App;
